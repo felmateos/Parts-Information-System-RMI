051e8fe3ef3e8693eb37cfb1df8a2eae0db0fe47
felmateos <felmateos@usp.br>
2022-06-15 12:41:32 -0300

    Improved Client-Server interaction by command lines.

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,3 +1,4 @@
+import java.net.SocketTimeoutException;
 import java.rmi.RemoteException;
 import java.rmi.registry.*;
 import java.util.LinkedList;
@@ -10,56 +11,71 @@ public class Client {
     private static PartRepository currentRepo = null;
     private static List<PartQuant> currentSubPartsList = new LinkedList<>();
     private static Scanner scanner = new Scanner(System.in);
+    private static String host = "127.0.0.1";
+    private static boolean running = false;
 
     public enum actions {
-        BIND("bind"), LISTP("listp");
-        
-        private final String valor;
-
-        actions (String valorOpcao) {
-            valor = valorOpcao;
-        }
-        public String getValor() {
-            return valor;
-        }
+        HELP, BIND, LISTP, QUIT;
     }
 
     public static void main(String[] args) throws RemoteException {
-        teste();
-        //executeClient();
+        //teste();
+        executeClient();
     }
 
-    private static void executeClient() {
+    private static void executeClient() throws RemoteException {
+        running = true;
         System.out.println("Ola! Este eh um sistema de informacoes de pecas distribuido.");
         System.out.println("Caso precise de ajuda digite \"help\".");
-        while (true) {
-            String action = "";
-            System.out.println("O que deseja fazer?");
-            action = scanner.nextLine();
-            String[] actionSplit = action.split(" ");
-            if (action.contains(actions.BIND.getValor())) 
-                bind(actionSplit[actionSplit.length-2],
-                 "127.0.0.1", 
-                 Integer.parseInt(actionSplit[actionSplit.length-1]));
-            break;
+        while (running) {
+            try {
+                String action = "";
+
+                System.out.println("O que deseja fazer?");
+                action = scanner.nextLine();
+
+                String[] actionSplit = action.split(" ");
+
+                actions chosenAction = actions.valueOf(actionSplit[0].toUpperCase());
+                
+                switch (chosenAction) {
+                    case HELP  -> help();
+                    case BIND  -> bind(actionSplit[1], Integer.parseInt(actionSplit[2]));
+                    case LISTP -> listp();
+                    case QUIT  -> running = false;
+                }
+            } catch (IllegalArgumentException iae) {
+                System.out.println("Comando invalido!");
+            }
         }
     }
 
-    private static void bind(String repoName, String host, int port) {
+    private static void help() {
+        System.out.println("help commands");
+    }
+
+    private static void bind(String repoName, int port) {
         try {
             
             Registry remoteRegistry = LocateRegistry.getRegistry(host, port);
             currentRepo = (PartRepository) remoteRegistry.lookup(repoName);
 
-            System.out.println("current repo: " + currentRepo.getName());
+            System.out.println("repositorio atual: " + currentRepo.getName() + " qtd. de pecas: " + currentRepo.getPartsQuant());
             
         } catch (Exception e) {
             System.out.println("ERRO NO CLIENTE:\n" + e.toString());
         }
     }
 
+    private static void listp() throws RemoteException{
+        System.out.println("\nInfos de todas as pecas: ");
+        System.out.println("Codigo | Nome |  Descricao  | Repositorio | SubPecas [Cod,Repo,Quant]");
+        for (int partCode : currentRepo.getAllPartsCodes())
+            System.out.println(((Part) currentRepo.getPartRemoteByCode(partCode)).getInfo());
+    }
+
     private static void teste() throws RemoteException{
-        bind("repo1", "127.0.0.1", 4001);
+        bind("repo1", 4001);
         System.out.println("Nome do repositório: " + currentRepo.getName());
 
         currentPart = (Part) currentRepo.getPartRemoteByCode(1);
@@ -100,7 +116,7 @@ public class Client {
             System.out.println(((Part) currentRepo.getPartRemoteByCode(partCode)).getInfo());
 
 
-        bind("repo2", "127.0.0.1", 4002);
+        bind("repo2", 4002);
         System.out.println("Nome do repositório: " + currentRepo.getName());
 
         System.out.println("\nInfos de todas as pecas: ");
